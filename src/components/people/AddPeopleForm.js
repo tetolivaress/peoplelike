8f7aa4a4047fd8e813a8b954ee65be684bf8fc86
tetolivares <tetolivares@gmail.com>
2021-05-08 16:53:59 -0400

    responsibe form

--- src/components/people/AddPeopleForm.js
+++ src/components/people/AddPeopleForm.js
@@ -1,23 +1,25 @@
 const AddPeopleForm = ({ form: { image, name }, onChange, onSubmit }) => {
 
   return (
-    <form onSubmit={onSubmit} className="flex flex-col m-12">
-      <input
-        type="file"
-        name="image"
-        placeholder="image"
-        onChange={onChange}
-        className="block bg-gray-300 p-2 rounded-md"
-      />
-      <input
-        type="text"
-        name="name"
-        placeholder="name"
-        value={name}
-        onChange={onChange}
-        className="block bg-gray-300 p-2 my-2 rounded-md"
-      />
-      <button className="bg-green-500 p-2 rounded-md font-bold">Guardar</button>
+    <form onSubmit={onSubmit} className="grid grid-cols-1 md:grid-cols-3 m-12">
+      <div>
+        <input
+          type="file"
+          name="image"
+          placeholder="image"
+          onChange={onChange}
+          className="block bg-gray-300 p-2 rounded-md w-full"
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={onChange}
+          className="block bg-gray-300 p-2 my-2 rounded-md w-full"
+        />
+        <button className="bg-green-500 p-2 rounded-md font-bold w-full">Guardar</button>
+      </div>
     </form>
   )
 }
--- src/components/people/Person.js
+++ src/components/people/Person.js
@@ -5,7 +5,7 @@ const Person = ({ person }) => {
     <div className="grid grid-cols-4 gap-4 items-center justify-center">
       <img src={person.image} alt={person.name} className="rounded-md max-h-24" />
       <div className="col-span-3">
-        <h1>{person.name}</h1>
+        <h1 className="font-bold">{person.name}</h1>
         <h1><ThumbUpIcon className="w-4 h-4 inline" /> {person.like}</h1>
         <h1><ThumbDownIcon className="w-4 h-4 inline" /> {person.dislike}</h1>
       </div>
